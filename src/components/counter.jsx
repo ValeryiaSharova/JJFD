fd575c6247f3162116ee6b2a0da69912a14e11a3
Aller-Forst <gominzyplay@gmail.com>
2021-12-06 22:53:09 +0300

    Code refactoring: CountersList components added.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,43 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const Counter = props => {
+  const [value, setValue] = useState(props.value);
 
-  const formatCount = () => {
-    return count === 0 ? 'empty' : count;
+  const formatValue = () => {
+    return value === 0 ? 'empty' : value;
   };
   const getBageClasses = () => {
     let classes = 'badge m-2 ';
-    classes += count === 0 ? 'bg-warning' : 'bg-primary';
+    classes += value === 0 ? 'bg-warning' : 'bg-primary';
     return classes;
   };
 
   const handleIncrement = () => {
-    setCount(prevState => prevState + 1);
+    setValue(prevState => prevState + 1);
   };
   const handleDecrement = () => {
-    setCount(prevState => prevState - 1);
+    setValue(prevState => prevState - 1);
   };
 
   return (
-    <>
-      <span className={getBageClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" type="button" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" type="button" onClick={handleDecrement}>
         -
       </button>
-    </>
+    </div>
   );
+};
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Counter;
