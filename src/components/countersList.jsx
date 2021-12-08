cad48af9d6066ebe54fc94bf4c09f4a5e9281df9
Aller-Forst <gominzyplay@gmail.com>
2021-12-08 21:16:01 +0300

    Initial state added.

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Counter from './counter';
 
 const CountersList = () => {
-  const counters = [
+  const initialState = [
     { id: 0, value: 0, name: 'Ненужная вещь' },
     { id: 1, value: 4, name: 'Ложка' },
     { id: 2, value: 0, name: 'Вилка' },
     { id: 3, value: 4, name: 'Тарелка' },
     { id: 4, value: 0, name: 'Набор минималиста' },
   ];
+  const [counters, setCounters] = useState(initialState);
   return (
     <>
       {counters.map(count => (
