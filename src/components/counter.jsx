2f7304113bbed3de2cf14a89444ffe0864b59ab7
Aller-Forst <gominzyplay@gmail.com>
2021-12-08 22:32:39 +0300

    Function for reset added. Function for incrementing and deleting moved from another component.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Counter = props => {
-  const [value, setValue] = useState(props.value);
-
+const Counter = ({ id, value, name, onHandleIncrement, onHandleDecrement, onDelete }) => {
   const formatValue = () => {
     return value === 0 ? 'empty' : value;
   };
@@ -14,22 +12,26 @@ const Counter = props => {
     return classes;
   };
 
-  const handleIncrement = () => {
-    setValue(prevState => prevState + 1);
-  };
-  const handleDecrement = () => {
-    setValue(prevState => prevState - 1);
-  };
-
   return (
     <div>
-      <span>{props.name}</span>
+      <span>{name}</span>
       <span className={getBageClasses()}>{formatValue()}</span>
-      <button className="btn btn-primary btn-sm m-2" type="button" onClick={handleIncrement}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        type="button"
+        onClick={() => onHandleIncrement(id)}
+      >
         +
       </button>
-      <button className="btn btn-primary btn-sm m-2" type="button" onClick={handleDecrement}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        type="button"
+        onClick={() => onHandleDecrement(id)}
+      >
         -
+      </button>
+      <button className="btn btn-danger btn-sm m-2" type="button" onClick={() => onDelete(id)}>
+        Delete
       </button>
     </div>
   );
@@ -38,6 +40,10 @@ const Counter = props => {
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onHandleIncrement: PropTypes.func.isRequired,
+  onHandleDecrement: PropTypes.func.isRequired,
 };
 
 export default Counter;
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import React, { useState } from 'react';
 import Counter from './counter';
 
 const CountersList = () => {
@@ -10,11 +11,45 @@ const CountersList = () => {
     { id: 4, value: 0, name: 'Набор минималиста' },
   ];
   const [counters, setCounters] = useState(initialState);
+  const handleDelete = id => {
+    const newCounters = counters.filter(counter => counter.id !== id);
+    setCounters(newCounters);
+  };
+  const handleReset = () => {
+    setCounters(initialState);
+  };
+  const handleIncrement = id => {
+    const updateCounters = counters.map(counter => {
+      if (counter.id === id) {
+        counter.value++;
+      }
+      return counter;
+    });
+    setCounters(updateCounters);
+  };
+  const handleDecrement = id => {
+    const updateCounters = counters.map(counter => {
+      if (counter.id === id) {
+        counter.value--;
+      }
+      return counter;
+    });
+    setCounters(updateCounters);
+  };
   return (
     <>
       {counters.map(count => (
-        <Counter key={count.id} value={count.value} name={count.name} />
+        <Counter
+          key={count.id}
+          {...count}
+          onDelete={handleDelete}
+          onHandleIncrement={handleIncrement}
+          onHandleDecrement={handleDecrement}
+        />
       ))}
+      <button className="btn btn-primary btn-sm m-2" type="button" onClick={handleReset}>
+        Reset
+      </button>
     </>
   );
 };
