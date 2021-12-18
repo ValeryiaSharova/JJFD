8b2fd72123c42a3936f24e9b07888aa3d357df4c
Aller-Forst <gominzyplay@gmail.com>
2021-12-18 22:08:29 +0300

    Fake api changed.

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
-import * as users from './fake.api/user.api';
+/* eslint-disable import/no-named-as-default */
+import users from './fake.api/user.api';
+import professions from './fake.api/professions.api';
 
 const API = {
   users,
+  professions,
 };
 export default API;
