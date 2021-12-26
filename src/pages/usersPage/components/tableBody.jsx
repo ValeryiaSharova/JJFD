6f79393b3f5ef538b8f75bf14638fc5b6a5427d9
Aller-Forst <gominzyplay@gmail.com>
2021-12-26 14:52:13 +0300

    Component TableBody added.

---
+++ src/pages/usersPage/components/tableBody.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+const TableBody = ({ data, columns }) => {
+  const renderContent = (item, column) => {
+    if (columns[column].component) {
+      const { component } = columns[column];
+      if (typeof component === 'function') {
+        return component(item);
+      }
+      return component;
+    }
+    return _.get(item, columns[column].path);
+  };
+  return (
+    <tbody>
+      {data.map(item => (
+        <tr className="center-elem" key={item._id}>
+          {Object.keys(columns).map(column => (
+            <td key={column}>{renderContent(item, column)}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+TableBody.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.object.isRequired,
+};
+
+export default TableBody;
