dc85bb989e220f246bd2be358e063787fde6390e
Aller-Forst <gominzyplay@gmail.com>
2022-01-11 00:17:01 +0300

    Code refactoring: react-router-dom added. Header created. New pages added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/loginPage/login';
+import Main from './pages/mainPage/main';
 import Users from './pages/usersPage/users';
+import Header from './sharedComponents/header';
 
-const App = () => <Users />;
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/login" component={Login} />
+      <Route path="/users" component={Users} />
+    </Switch>
+  </>
+);
 export default App;
---
+++ src/sharedComponents/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className="nav">
+      <Link className="nav-link active" to="/">
+        Main
+      </Link>
+      <Link className="nav-link" to="/login">
+        Login
+      </Link>
+      <Link className="nav-link" to="/users">
+        Users
+      </Link>
+    </nav>
+  );
+};
+
+export default Header;
