9f1cf2fbcc443566b64826ae966dc3545e215803
Aller-Forst <gominzyplay@gmail.com>
2022-01-11 22:51:05 +0300

    Path to component changed. Route for user page changed.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/loginPage/login';
 import Main from './pages/mainPage/main';
-import Users from './pages/usersPage/users';
+import Users from './pages/usersPage/usersPage';
 import Header from './sharedComponents/header';
 
 const App = () => (
@@ -11,7 +11,7 @@ const App = () => (
     <Switch>
       <Route path="/" exact component={Main} />
       <Route path="/login" component={Login} />
-      <Route path="/users" component={Users} />
+      <Route path="/users/:userId?" component={Users} />
     </Switch>
   </>
 );
