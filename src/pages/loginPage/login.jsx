7adeb4f23bf2b944b58a0ba426ef982ab748fb07
Aller-Forst <gominzyplay@gmail.com>
2022-01-11 23:52:55 +0300

    Form for login added.

--- src/pages/loginPage/login.jsx
+++ src/pages/loginPage/login.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from './components/textField';
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [data, setData] = useState({ email: '', password: '' });
+  const handleChange = ({ target }) => {
+    setData(prevState => ({ ...prevState, [target.name]: target.value }));
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Подтвердить</button>
+    </form>
+  );
 };
 
 export default Login;
