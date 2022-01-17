7acbdc3bc29e273d06a739d6027f1f58f8c3675d
Aller-Forst <gominzyplay@gmail.com>
2022-01-17 22:01:21 +0300

    Code refactoring: login page changed.

--- src/pages/loginPage/login.jsx
+++ src/pages/loginPage/login.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 
 const Login = () => {
-  return <LoginForm />;
+  const { type } = useParams();
+  const [formType, setFormType] = useState(type === 'register' ? type : 'login');
+  const toggleFormType = () => {
+    setFormType(prevState => (prevState === 'register' ? 'login' : 'register'));
+  };
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 shadow p-4">
+          {formType === 'register' ? (
+            <>
+              <h3 className="mb-4">Регистрация</h3>
+              <RegisterForm />
+              <p>
+                Уже есть аккаунт?{' '}
+                <a role="button" onClick={toggleFormType}>
+                  Вход
+                </a>
+              </p>
+            </>
+          ) : (
+            <>
+              <h3 className="mb-4">Вход</h3>
+              <LoginForm />
+              <p>
+                Нет аккаунта?{' '}
+                <a role="button" onClick={toggleFormType}>
+                  Регистрация
+                </a>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
