0ad25adffbfa3d7524a27c87c52ea3add4470b20
Aller-Forst <gominzyplay@gmail.com>
2022-01-19 21:01:49 +0300

    useRef added.

--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+    const handleClick = () => {
+        console.log(inputRef.current);
+        inputRef.current.focus();
+    };
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                ref={inputRef}
+                type="email"
+                className="form-control"
+                id="email"
+            />
+            <button className="btn btn-danger" onClick={handleClick}>
+                button
+            </button>
         </CardWrapper>
     );
 };
