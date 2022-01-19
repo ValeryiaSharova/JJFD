ed35a0a0b472a3047ebf6ccdbe98a85f9bcd43e0
Aller-Forst <gominzyplay@gmail.com>
2022-01-19 21:02:04 +0300

    useMemo added.

--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1;
+}
+
+function runFactorial(n) {
+    console.log("run");
+    return factorial(n);
+}
+
 const ComplexCalculateExample = () => {
+    const [value, setValue] = useState(100);
+    const [otherState, setOtherState] = useState(false);
+
+    const buttonColor = otherState ? "danger" : "secondary";
+    useEffect(() => {
+        console.log("render");
+    });
+
+    const fact = useMemo(() => runFactorial(value), [value]);
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p> value {value}</p>
+                <p>Result fact {fact}</p>
+                <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => setValue((prevState) => prevState + 10)}
+                >
+                    Increment
+                </button>
+                <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => setValue((prevState) => prevState - 10)}
+                >
+                    Decrement
+                </button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <button
+                    className={`btn btn-${buttonColor} ms-md-2`}
+                    onClick={() => setOtherState((prevState) => !prevState)}
+                >
+                    change
+                </button>
             </CardWrapper>
         </>
     );
