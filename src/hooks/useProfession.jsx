ce1ae1c930875d02103e093ebe6f0533c30c07b4
Aller-Forst <gominzyplay@gmail.com>
2022-02-02 00:15:32 +0300

    Professions service added. Custom hook for professions added.

---
+++ src/hooks/useProfession.jsx
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import professionService from '../services/profession.service';
+
+const ProfessionContext = React.createContext();
+
+export const useProfessions = () => {
+  return useContext(ProfessionContext);
+};
+
+export const ProfessionProvider = ({ children }) => {
+  const [profession, setProfession] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function getProfessionsList() {
+    try {
+      const { content } = await professionService.get();
+      setProfession(content);
+      setLoading(false);
+    } catch (error) {
+      errorCatcher(error);
+    }
+  }
+
+  function getProfession(id) {
+    return profession.find(p => p._id === id);
+  }
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  }
+
+  useEffect(() => {
+    getProfessionsList();
+  }, []);
+
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error);
+      setError(null);
+    }
+  }, [error]);
+
+  return (
+    <ProfessionContext.Provider value={{ isLoading, profession, getProfession }}>
+      {children}
+    </ProfessionContext.Provider>
+  );
+};
+
+ProfessionProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+};
