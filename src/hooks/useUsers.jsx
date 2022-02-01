18319ebfbe340ce17983843c789533006332abf6
Aller-Forst <gominzyplay@gmail.com>
2022-02-02 00:14:56 +0300

    User service added. Custom hook for users added.

---
+++ src/hooks/useUsers.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import userService from '../services/user.service';
+
+const UserContext = React.createContext();
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
+
+const UserProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function getUsers() {
+    try {
+      const { content } = await userService.get();
+      setUsers(content);
+      setLoading(false);
+    } catch (error) {
+      errorCatcher(error);
+    }
+  }
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error);
+      setError(null);
+    }
+  }, [error]);
+
+  return (
+    <UserContext.Provider value={{ users }}>
+      {!isLoading ? children : <h1>Загрузка...</h1>}
+    </UserContext.Provider>
+  );
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+};
+
+export default UserProvider;
