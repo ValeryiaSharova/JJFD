70eb78f3b7cc63efff2a8f9036a65be5af13d66e
Aller-Forst <gominzyplay@gmail.com>
2022-02-16 00:07:55 +0300

    Header changed. New navigation item added.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import NavProfile from './navProfile';
 
 const Header = () => {
+  const { currentUser } = useAuth();
   return (
-    <nav className="nav">
-      <Link className="nav-link" to="/">
-        Главная
-      </Link>
-      <Link className="nav-link" to="/login">
-        Вход
-      </Link>
-      <Link className="nav-link" to="/users">
-        Пользователи
-      </Link>
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Главная
+            </Link>
+          </li>
+          {currentUser && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Пользователи
+              </Link>
+            </li>
+          )}
+        </ul>
+        <div className="d-flex">
+          {currentUser ? (
+            <NavProfile />
+          ) : (
+            <Link className="nav-link" to="/login">
+              Вход
+            </Link>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
