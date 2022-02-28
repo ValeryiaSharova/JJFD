596fe84c74deee1930ea7957b4c88a76163f7a61
Aller-Forst <gominzyplay@gmail.com>
2022-03-01 00:36:26 +0300

    Professions reducer added to store.

--- src/App.js
+++ src/App.js
@@ -7,29 +7,28 @@ import Main from './pages/mainPage/main';
 import Users from './pages/usersPage/usersPage';
 import Header from './components/header';
 import UserEdit from './pages/userPage/userEdit';
-import { ProfessionProvider } from './hooks/useProfession';
 import AuthProvider from './hooks/useAuth';
 import ProtectedRoute from './components/protectedRoute';
 import LogOut from './components/logOut';
 import { loadQualitiesList } from './store/qualities';
+import { loadProfessionsList } from './store/profession';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadQualitiesList());
+    dispatch(loadProfessionsList());
   }, []);
   return (
     <>
       <AuthProvider>
         <Header />
         <Switch>
-          <ProfessionProvider>
-            <Route path="/" exact component={Main} />
-            <Route path="/logout" component={LogOut} />
-            <Route path="/login/:type?" component={Login} />
-            <ProtectedRoute path="/users/:userId?" exact component={Users} />
-            <ProtectedRoute path="/users/:userId?/edit" exact component={UserEdit} />
-          </ProfessionProvider>
+          <Route path="/" exact component={Main} />
+          <Route path="/logout" component={LogOut} />
+          <Route path="/login/:type?" component={Login} />
+          <ProtectedRoute path="/users/:userId?" exact component={Users} />
+          <ProtectedRoute path="/users/:userId?/edit" exact component={UserEdit} />
         </Switch>
       </AuthProvider>
       <ToastContainer />
