c804ef85e42c0bc5cc7b02327c1e172aab9b63b3
Aller-Forst <gominzyplay@gmail.com>
2022-02-28 23:50:14 +0300

    Redux store created and added to project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 import Login from './pages/loginPage/login';
 import Main from './pages/mainPage/main';
 import Users from './pages/usersPage/usersPage';
 import Header from './components/header';
 import UserEdit from './pages/userPage/userEdit';
 import { ProfessionProvider } from './hooks/useProfession';
-import { QualitiesProvider } from './hooks/useQualities';
 import AuthProvider from './hooks/useAuth';
 import ProtectedRoute from './components/protectedRoute';
 import LogOut from './components/logOut';
+import { loadQualitiesList } from './store/qualities';
 
-const App = () => (
-  <>
-    <AuthProvider>
-      <Header />
-      <Switch>
-        <QualitiesProvider>
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadQualitiesList());
+  }, []);
+  return (
+    <>
+      <AuthProvider>
+        <Header />
+        <Switch>
           <ProfessionProvider>
             <Route path="/" exact component={Main} />
             <Route path="/logout" component={LogOut} />
@@ -25,10 +30,10 @@ const App = () => (
             <ProtectedRoute path="/users/:userId?" exact component={Users} />
             <ProtectedRoute path="/users/:userId?/edit" exact component={UserEdit} />
           </ProfessionProvider>
-        </QualitiesProvider>
-      </Switch>
-    </AuthProvider>
-    <ToastContainer />
-  </>
-);
+        </Switch>
+      </AuthProvider>
+      <ToastContainer />
+    </>
+  );
+};
 export default App;
