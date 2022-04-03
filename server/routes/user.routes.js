9901528437e04e6d2b912dac87fa2adb80d4e7dd
Aller-Forst <gominzyplay@gmail.com>
2022-04-03 23:12:42 +0300

    Logic for user's entity was added on backend.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,36 @@
 const express = require("express");
+const User = require("../models/User");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:id", auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "На сервере произошла ошибка. Попробуйте позже" });
+  }
+});
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const list = await User.find();
+    res.status(200).send(list);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "На сервере произошла ошибка. Попробуйте позже" });
+  }
+});
 
 module.exports = router;
